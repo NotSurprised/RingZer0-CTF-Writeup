7ec395993059b2c10168d404c51d11fc6caabeaf
NotSurprised <roro8016489@gmail.com>
2018-09-14 01:41:10 +0800

    20180914

---
+++ JailEscaping/C Jail/C Jail 3/Level 3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int main(int argc, char **argv) {
+	%:include "/home/level3/flag.txt"
+}
